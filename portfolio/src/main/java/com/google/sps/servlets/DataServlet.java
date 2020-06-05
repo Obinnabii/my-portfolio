fbd9843583de6b28467604aad4cae9bd6aab798d
Obi Abii <obinna.abii@gmail.com>
2020-06-05 01:11:09 +0000

    modify serverlet GET function to match changes

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,7 +17,11 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
+import com.google.sps.data.Comment;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.sql.Timestamp;
@@ -30,50 +34,45 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/comments")
 public class DataServlet extends HttpServlet {
 
-  private ArrayList<String> comments;
-
-  @Override
-  public void init() {
-    comments = new ArrayList<String>();
-    comments.add("Wow, Obi nice website!");
-    comments.add("I love the color scheme");
-    comments.add("We love you Obi!");
-  }
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String json = convertToJson(comments);
-    
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<Comment> commentList = new ArrayList<>();
+    for (Entity entity : results.asIterable()) {
+      long id = entity.getKey().getId();
+      String text = (String) entity.getProperty("text");
+      long timestamp = (long) entity.getProperty("timestamp");
+
+      Comment comment = new Comment(id, text, timestamp);
+      commentList.add(comment);
+    }
+
+    Gson gson = new Gson();
+
     response.setContentType("application/json;");
-    response.getWriter().println(json);
+    response.getWriter().println(gson.toJson(commentList));
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
-    String comment = request.getParameter("comment-input");
-    if (!comment.isBlank()) { 
+    String text = request.getParameter("comment-input");
+    if (!text.isBlank()) { 
       long timestamp = System.currentTimeMillis();
       Entity commentEntity = new Entity("Comment");
 
-      commentEntity.setProperty("comment", comment);
+      commentEntity.setProperty("text", text);
       commentEntity.setProperty("timestamp", timestamp);
 
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(commentEntity);
-      comments.add(comment);
     }
     
     // Redirect back to page
     response.sendRedirect("comments.html");
-  }
-
-    /**
-   * Converts a ArrayList<String> instance into a JSON string
-   */
-  private String convertToJson(ArrayList<String> arrList) {
-    Gson gson = new Gson();
-    String json = gson.toJson(arrList);
-    return json;
   }
 }
