ba91c293d2825ceb51887650104b7b0b53c05517
Obi Abii <obinna.abii@gmail.com>
2020-06-11 01:12:53 -0400

    add functionality to delete all comments to the backend

--- portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteCommentServlet.java
@@ -17,6 +17,11 @@ package com.google.sps.servlets;
 import com.google.sps.data.Comment;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import java.io.IOException;
@@ -31,10 +36,18 @@ public class DeleteCommentServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long commentId = Long.parseLong(request.getParameter("id"));
-
-    Key commentEntityKey = KeyFactory.createKey(Comment.ENTITY_NAME, commentId);
+    boolean deleteAll = Boolean.parseBoolean(request.getParameter("deleteAll"));
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    datastore.delete(commentEntityKey);
+    if(!deleteAll){
+      long commentId = Long.parseLong(request.getParameter("id"));
+      Key commentEntityKey = KeyFactory.createKey(Comment.ENTITY_NAME, commentId);
+      datastore.delete(commentEntityKey);
+    }else{
+      Query query = new Query(Comment.ENTITY_NAME).setKeysOnly();
+      PreparedQuery queryResults = datastore.prepare(query);
+      for (Entity commentEntity : queryResults.asIterable()) {
+        datastore.delete(commentEntity.getKey());
+      }
+    }
   }
 }
